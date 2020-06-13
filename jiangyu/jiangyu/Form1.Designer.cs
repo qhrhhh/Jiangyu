311e7568c77f9f9eaab3e812df762e5a6143df65
home <2453042508@qq.com>
2020-06-13 17:55:41 +0800

    aa

---
+++ jiangyu/jiangyu/Form1.Designer.cs
@@ -0,0 +1,240 @@
+﻿namespace jiangyu
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.label2 = new System.Windows.Forms.Label();
+            this.yearb = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.yeare = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.qsyl = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(856, 48);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 29);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "导入";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(32, 54);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(37, 15);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "文件";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(135, 49);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(680, 25);
+            this.textBox1.TabIndex = 2;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(35, 92);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(37, 15);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "年份";
+            // 
+            // yearb
+            // 
+            this.yearb.Location = new System.Drawing.Point(135, 92);
+            this.yearb.Margin = new System.Windows.Forms.Padding(4);
+            this.yearb.MaxLength = 4;
+            this.yearb.Name = "yearb";
+            this.yearb.Size = new System.Drawing.Size(132, 25);
+            this.yearb.TabIndex = 4;
+            this.yearb.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.yearb_KeyPress);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(296, 102);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(22, 15);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "至";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(584, 102);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(22, 15);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "年";
+            // 
+            // yeare
+            // 
+            this.yeare.Location = new System.Drawing.Point(387, 91);
+            this.yeare.Margin = new System.Windows.Forms.Padding(4);
+            this.yeare.MaxLength = 4;
+            this.yeare.Name = "yeare";
+            this.yeare.Size = new System.Drawing.Size(132, 25);
+            this.yeare.TabIndex = 7;
+            this.yeare.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.yeare_KeyPress);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(35, 140);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(67, 15);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "侵蚀雨量";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(296, 140);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(23, 15);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "mm";
+            // 
+            // qsyl
+            // 
+            this.qsyl.Location = new System.Drawing.Point(135, 140);
+            this.qsyl.Margin = new System.Windows.Forms.Padding(4);
+            this.qsyl.Name = "qsyl";
+            this.qsyl.Size = new System.Drawing.Size(132, 25);
+            this.qsyl.TabIndex = 9;
+            this.qsyl.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox2_KeyPress);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(37, 182);
+            this.label7.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(37, 15);
+            this.label7.TabIndex = 10;
+            this.label7.Text = "求值";
+            this.label7.Visible = false;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 15;
+            this.listBox1.Location = new System.Drawing.Point(135, 182);
+            this.listBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(680, 109);
+            this.listBox1.TabIndex = 11;
+            this.listBox1.Visible = false;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(417, 470);
+            this.button2.Margin = new System.Windows.Forms.Padding(4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(100, 29);
+            this.button2.TabIndex = 12;
+            this.button2.Text = "计算";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1069, 622);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.qsyl);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.yeare);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.yearb);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox yearb;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox yeare;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox qsyl;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ jiangyu/jiangyu/Form1.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+namespace jiangyu
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private String fileName;
+        private List<String> jyList = new List<string>();
+        private int sumjy = 0;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                textBox1.Text = openFileDialog1.FileName;
+                this.fileName = openFileDialog1.FileName;
+            }
+            
+        }
+        public void WriteTxt(String path,String txt)
+        {
+            FileStream fs = new FileStream(path, FileMode.Append,FileAccess.Write);
+            StreamWriter sw = new StreamWriter(fs);
+            //开始写入
+            sw.Write(txt);
+            sw.Close();
+            fs.Close();
+        }
+      
+        private void yearb_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if ((e.KeyChar <= 47 || e.KeyChar >= 58) && (e.KeyChar != 8) && (e.KeyChar != 46))
+                e.Handled = true;
+        }
+
+        private void yeare_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if ((e.KeyChar <= 47 || e.KeyChar >= 58) && (e.KeyChar != 8) && (e.KeyChar != 46))
+                e.Handled = true;
+        }
+
+        private void textBox2_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if ((e.KeyChar <= 47 || e.KeyChar >= 58) && (e.KeyChar != 8) && (e.KeyChar != 46))
+                e.Handled = true;
+            if (e.KeyChar == 46)                       //小数点
+            {
+                if (qsyl.Text.Length <= 0)
+                    e.Handled = true;           //小数点不能在第一位
+                else
+                {
+                    float f;
+                    float oldf;
+                    bool b1 = false, b2 = false;
+                    b1 = float.TryParse(qsyl.Text, out oldf);
+                    b2 = float.TryParse(qsyl.Text + e.KeyChar.ToString(), out f);
+                    if (b2 == false)
+                    {
+                        if (b1 == true)
+                            e.Handled = true;
+                        else
+                            e.Handled = false;
+                    }
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            StreamReader sr = new StreamReader(fileName, Encoding.Default);
+            string line;
+            List<JYData> l = new List<JYData>();
+            int yearnum = int.Parse(yeare.Text) - int.Parse(yearb.Text) + 1;
+            double[,] yjy = new double[yearnum,25];
+            double[,] qsjy = new double[yearnum, 25];
+            double[] mjy = new double[yjy.Length * 2];
+            double[,] yuedaym = new double[yearnum, 25];
+            line = sr.ReadLine();
+            int ryln = 0;
+            while ((line = sr.ReadLine()) != null)
+            {
+                jyList.Add(line);
+                String[] data = line.Split('\t');
+                JYData jy = new JYData(data[0], "", data[1], data[2], data[3], data[4]);
+                l.Add(jy);
+                //int beginyear = int.Parse(yearb.Text);
+                if (int.Parse(data[1]) < int.Parse(yearb.Text)) continue;
+                if (int.Parse(data[1]) > int.Parse(yeare.Text)) continue;
+                yjy[int.Parse(data[1]) - int.Parse(yearb.Text),0] += double.Parse(data[4]);
+                if(double.Parse(data[4])>double.Parse(qsyl.Text))
+                    qsjy[int.Parse(data[1]) - int.Parse(yearb.Text), 0] += double.Parse(data[4]);
+                int index = (int.Parse(data[2]) - 1) * 2 + 1;
+                if (int.Parse(data[3]) > 15)
+                    index += 1;
+                if (double.Parse(data[4]) > double.Parse(qsyl.Text)) {
+                    ryln++;
+                    qsjy[int.Parse(data[1]) - int.Parse(yearb.Text), index] += double.Parse(data[4]);
+                }
+                yjy[int.Parse(data[1]) - int.Parse(yearb.Text), index] += double.Parse(data[4]);
+
+            }
+            double rylsum = 0;
+            double a = 0.3101; 
+            double rby = 0;
+            for (int i = 0; i < yearnum; i++)
+            {
+                rylsum += qsjy[i, 0];
+                for(int j = 1; j < 25; j++)
+                {
+                    if (j > 4 && j < 10) a = 0.3937;
+                    rby+=Math.Pow(Convert.ToDouble(qsjy[i, j]), a);
+                    
+                }
+            }
+            double rylp = rylsum / ryln;
+            double rylb = 0.6243 + (27.346 / rylp);
+            double ryla = 21.239 * Math.Pow(rylb, -7.3967);
+            WriteTxt("日雨量(自定义).txt", ryla.ToString()+"\t"+rylb+"\t"+rylp + "\n");
+            rby = rby / yearnum;            
+            for (int i = 0; i < yjy.GetLength(0); i++) {
+                String outString = (i + int.Parse(yearb.Text)).ToString() + "\t"+yjy[i, 0].ToString() + "\t";
+                String outqsString = (i + int.Parse(yearb.Text)).ToString() + "\t" + qsjy[i, 0].ToString() + "\t";
+                for (int j = 1; j < 25; j++)
+                {
+                    outString += yjy[i, j].ToString() + "\t";
+                    outqsString += qsjy[i, j].ToString() + "\t";
+                }
+
+                outqsString += rby.ToString() + "\t" +rby/qsjy[i,0]+ "\t";
+                WriteTxt("降雨量.txt", outString + "\n");
+                WriteTxt("侵蚀降雨量.txt", outqsString + "\n");
+            }
+            button2.Text = "完成";
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+    public class JYData
+    {
+        private String zdname;
+        private String zdnum;
+        private String year;
+        private String month;
+        private String day;
+        private String jyl;
+
+        public JYData(){
+            zdname = "";
+            zdnum = "";
+            year = "";
+            month = "";
+            day = "";
+            jyl = "";
+        }
+        public JYData(String a,String b,String c ,String d,String e,String f)
+        {
+            zdname =a;
+            zdnum = b;
+            year = c;
+            month = d;
+            day = e;
+            jyl = f;
+        }
+   
+        public string Zdname { get => zdname; set => zdname = value; }
+        public string Zdnum { get => zdnum; set => zdnum = value; }
+        public string Year { get => year; set => year = value; }
+        public string Month { get => month; set => month = value; }
+        public string Jyl { get => jyl; set => jyl = value; }
+        public string Day { get => day; set => day = value; }
+    }
+}
